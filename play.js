b8146528ca9edcfda6aa8c69a8b0249389f1c1cb
Rene Wang <rene.wang1@hotmail.com>
2020-12-02 18:08:32 +0000

    added exit functionality

--- play.js
+++ play.js
@@ -2,3 +2,22 @@ const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    const handleUserInput = function() {
+      if (key === '\u0003') {
+        process.exit();
+      }
+    }
+  });
+  return stdin;
+}
